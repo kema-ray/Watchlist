2e53bbd87a7b982c24e46922f471f4528170f734
kema-ray <rachelkemuma99@gmail.com>
2022-04-25 23:03:18 +0300

    create request  key

--- app/views.py
+++ app/views.py
@@ -1,12 +1,19 @@
-from email import message
 from flask import render_template
 from app import app
 
 #views
 @app.route('/')
 def index():
+
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Hello World'
-    return render_template('index.html',message=message)
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
+# @app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('movie.html',id = movie_id)
