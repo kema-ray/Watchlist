67a47393ced780a14b4e973fa4f08adcfb418227
rachel <rachelkemuma99@gmail.com>
2022-05-07 19:57:06 +0300

    add flask email

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,10 +1,10 @@
-from flask import render_template
+# from flask import render_template
 # from app import app
-from . import main
+# from . import main
 
-@main.app_errorhandler(404)
-def four_Ow_four(errors):
-    '''
-    Function to render the 404 error page
-    '''
-    return render_template('fourOwFour.html'),404
+# @main.app_errorhandler(404)
+# def four_Ow_four(errors):
+#     '''
+#     Function to render the 404 error page
+#     '''
+#     return render_template('fourOwFour.html'),404
--- config.py
+++ config.py
@@ -10,6 +10,14 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:hotspurs@localhost/watchlist'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 #os.environ.get function to get SECRET_KEY and MOVIE_API_KEY which we will set as environment variables.
 class ProdConfig(Config):
     '''
